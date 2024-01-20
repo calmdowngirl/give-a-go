62170be74a5875e9a78a0759552fab2ccf2f730c
calmdowngirl <116081897+calmdowngirl@users.noreply.github.com>
2024-01-20 18:23:36 +1100

    added footer and updated readme

--- README.md
+++ README.md
@@ -1,16 +1,4 @@
-# Fresh project
+# number signs
 
-Your new Fresh project is ready to go. You can follow the Fresh "Getting
-Started" guide here: https://fresh.deno.dev/docs/getting-started
-
-### Usage
-
-Make sure to install Deno: https://deno.land/manual/getting_started/installation
-
-Then start the project:
-
-```
-deno task start
-```
-
-This will watch the project directory and restart as necessary.
+the numbers sign pics are from [google search images](https://www.google.com/search?q=auslan+numbers), i picked and arranged them based on this video i watched:
+https://www.youtube.com/watch?v=U-UZKcBIJe0&t=89s
--- routes/index.tsx
+++ routes/index.tsx
@@ -1,28 +1,43 @@
 export default function Home() {
   return (
     <>
-    <div class="px-4 py-8 mx-auto bg-[#F5F4FB]">
-      <div class="max-w-screen-md mx-auto flex flex-col items-center justify-center">
-        <img
-          class="my-6"
-          src="/logo.svg"
-          width="128"
-          height="128"
-          alt="the Fresh logo: a sliced lemon dripping with juice"
-        />
-        <h1 class="text-4xl font-bold">give auslan a go</h1>
-        <p class="my-4">
-          <a target="_blank" href="https://en.wikipedia.org/wiki/Auslan" class="underline decoration-dotted">Auslan</a> (/ˈɒzlæn/) is the sign language used by the majority of the Australian Deaf community.
-        </p>
-      </div>
-    </div>
+    <div class="flex flex-col min-h-screen">
+      <div class="flex-1">
+        <div class="px-4 py-8 mx-auto bg-[#F5F4FB]">
+          <div class="max-w-screen-md mx-auto flex flex-col items-center justify-center">
+            <img
+              class="my-6"
+              src="/logo.svg"
+              width="128"
+              height="128"
+              alt="the Fresh logo: a sliced lemon dripping with juice"
+            />
+            <h1 class="text-4xl font-bold">give auslan a go</h1>
+            <p class="my-4">
+              <a target="_blank" href="https://en.wikipedia.org/wiki/Auslan" class="underline decoration-dotted">Auslan</a> (/ˈɒzlæn/) is the sign language used by the majority of the Australian Deaf community.
+            </p>
+          </div>
+        </div>
 
-    <div class="max-w-screen-md mx-auto mt-8 flex flex-col items-center justify-center">
-      <ul class="text-3xl font-thin">
-        <li><a href="/number" class="underline decoration-[#DAA1F5] italic">give numbers a go</a></li>
-        <li><a href="#" class="underline decoration-[#DAA1F5] italic">give letters a go (todo)</a></li>
-      </ul>
+        <div class="max-w-screen-md mx-auto mt-8 flex flex-col items-center justify-center">
+          <ul class="text-3xl font-thin">
+            <li><a href="/number" class="underline decoration-[#DAA1F5] italic">give numbers a go</a></li>
+            <li><a href="#" class="underline decoration-[#DAA1F5] italic">give letters a go (todo)</a></li>
+          </ul>
+        </div>
+      </div>
+      <footer>
+        <div class="max-w-screen-md mx-auto mb-1 flex flex-row items-center justify-center text-sm text-gray-300">
+          <a target="_blank" href="https://deafnav.com.au/understand/communication/auslan" class={getFooterLinkClass()}>more on auslan</a>
+          <span class="px-2">/</span>
+          <a target="_blank" href="https://github.com/calmdowngirl/give-a-go" class={getFooterLinkClass()}>this app on github</a>
+        </div>
+      </footer>
     </div>
     </>
   );
+}
+
+function getFooterLinkClass() {
+  return "hover:underline decoration-[#DAA1F5] hover:text-gray-400"
 }
